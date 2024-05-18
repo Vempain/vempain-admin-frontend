79823b984e38e0175decbfb2cd5e2a1bd4b180b7
Paul-Erik Törrönen <poltsi@poltsi.fi>
2024-05-18 10:18:21 +0300

    Run code formatting on TopBar.tsx

--- src/main/TopBar.tsx
+++ src/main/TopBar.tsx
@@ -1,5 +1,5 @@
-import { Button, Flex, Layout, Menu, MenuProps } from 'antd';
-import React, { useState } from 'react';
+import { Button, Flex, Layout, Menu, MenuProps } from "antd";
+import React, { useState } from "react";
 import {
     AppstoreOutlined,
     AudioOutlined,
@@ -20,144 +20,140 @@ import {
     UsergroupAddOutlined,
     UserOutlined,
     VideoCameraOutlined
-} from '@ant-design/icons';
-import { useSession } from '../session';
-import { Link } from 'react-router-dom';
+} from "@ant-design/icons";
+import { useSession } from "../session";
+import { Link } from "react-router-dom";
 
 const {Header} = Layout;
 
 function TopBar() {
-    const [current, setCurrent] = useState('mail');
+    const [current, setCurrent] = useState("mail");
     const {userSession} = useSession();
 
-    const mainMenuItems: MenuProps['items'] = [
+    const mainMenuItems: MenuProps["items"] = [
         {
-            label: 'Content Management',
-            key: 'pageManagement',
+            label: "Content Management",
+            key: "pageManagement",
             icon: <SnippetsOutlined/>,
             children: [
                 {
-                    label: (<Link to={'/layouts'}>Layout</Link>),
-                    key: 'layout',
+                    label: (<Link to={"/layouts"}>Layout</Link>),
+                    key: "layout",
                     icon: <FormOutlined/>
                 },
                 {
-                    label: (<Link to={'/components'}>Component</Link>),
-                    key: 'component',
+                    label: (<Link to={"/components"}>Component</Link>),
+                    key: "component",
                     icon: <AppstoreOutlined/>
                 },
                 {
-                    label: (<Link to={'/forms'}>Form</Link>),
-                    key: 'form',
+                    label: (<Link to={"/forms"}>Form</Link>),
+                    key: "form",
                     icon: <ContainerOutlined/>
                 },
                 {
-                    label: (<Link to={'/pages'}>Page</Link>),
-                    key: 'page',
+                    label: (<Link to={"/pages"}>Page</Link>),
+                    key: "page",
                     icon: <FileTextOutlined/>
-                },
-                {
-                    label: 'Test',
-                    key: 'test',
-                },
-            ],
+                }
+            ]
         },
         {
-            label: 'File Management',
-            key: 'fileManagement',
+            label: "File Management",
+            key: "fileManagement",
             icon: <FileOutlined/>,
             children: [
                 {
-                    label: (<Link to={'/audios'}>Audio</Link>),
-                    key: 'audio',
+                    label: (<Link to={"/audios"}>Audio</Link>),
+                    key: "audio",
                     icon: <AudioOutlined/>
                 },
                 {
-                    label: (<Link to={'/documents'}>Document</Link>),
-                    key: 'document',
+                    label: (<Link to={"/documents"}>Document</Link>),
+                    key: "document",
                     icon: <FileUnknownOutlined/>
                 },
                 {
-                    label: (<Link to={'/images'}>Image</Link>),
-                    key: 'image',
+                    label: (<Link to={"/images"}>Image</Link>),
+                    key: "image",
                     icon: <FileImageOutlined/>
                 },
                 {
-                    label: (<Link to={'/videos'}>Video</Link>),
-                    key: 'video',
+                    label: (<Link to={"/videos"}>Video</Link>),
+                    key: "video",
                     icon: <VideoCameraOutlined/>
                 },
                 {
-                    label: (<Link to={'/galleries'}>Gallery</Link>),
-                    key: 'gallery',
+                    label: (<Link to={"/galleries"}>Gallery</Link>),
+                    key: "gallery",
                     icon: <PictureOutlined/>
                 },
                 {
-                    label: (<Link to={'/import'}>File import</Link>),
-                    key: 'import',
+                    label: (<Link to={"/import"}>File import</Link>),
+                    key: "import",
                     icon: <ImportOutlined/>
                 },
             ],
         },
         {
-            label: 'User Management',
-            key: 'userManagement',
+            label: "User Management",
+            key: "userManagement",
             icon: <UserOutlined/>,
             children: [
                 {
-                    label: (<Link to={'/users'}>Users</Link>),
-                    key: 'users',
+                    label: (<Link to={"/users"}>Users</Link>),
+                    key: "users",
                     icon: <UserAddOutlined/>
                 },
                 {
-                    label: (<Link to={'/units'}>Units</Link>),
-                    key: 'units',
+                    label: (<Link to={"/units"}>Units</Link>),
+                    key: "units",
                     icon: <UsergroupAddOutlined/>
                 },
             ],
         },
     ];
 
-    const userMenuItems: MenuProps['items'] = [
+    const userMenuItems: MenuProps["items"] = [
         {
-            label: 'Profile',
-            key: 'profile',
+            label: "Profile",
+            key: "profile",
             icon: <InfoCircleOutlined/>,
             children: [
                 {
-                    label: 'Account',
-                    key: 'account',
+                    label: "Account",
+                    key: "account",
                     icon: <SettingFilled/>
                 },
                 {
-                    label: 'Change Password',
-                    key: 'changePassword',
+                    label: "Change Password",
+                    key: "changePassword",
                     icon: <SwapOutlined/>
                 },
                 {
-                    label: (<Link to={'/logout'}>Log out</Link>),
-                    key: 'logout',
+                    label: (<Link to={"/logout"}>Log out</Link>),
+                    key: "logout",
                     icon: <LogoutOutlined/>
                 }
             ]
         }
     ];
 
-    const onClick: MenuProps['onClick'] = (e) => {
+    const onClick: MenuProps["onClick"] = (e) => {
         setCurrent(e.key);
     };
 
     return (
-            <Header style={{display: 'flex', alignItems: 'center'}}>
-                <div className='demo-logo'/>
-                {userSession && <Flex gap={'middle'} vertical={false}>
-                    <Menu onClick={onClick} selectedKeys={[current]} mode='horizontal' items={mainMenuItems}/>
+            <Header style={{display: "flex", alignItems: "center"}}>
+                <div className="demo-logo"/>
+                {userSession && <Flex gap={"middle"} vertical={false}>
+                    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={mainMenuItems}/>
                 </Flex>}
-                <div style={{marginLeft: 'auto'}}>
+                <div style={{marginLeft: "auto"}}>
                     {userSession ? (
-                            <Menu mode={'horizontal'} items={userMenuItems}/>
+                            <Menu mode={"horizontal"} items={userMenuItems}/>
                     ) : (
-                            <Button type={'text'} href={'/login'}>
+                            <Button type={"text"} href={"/login"}>
                                 Login
                             </Button>
                     )}
