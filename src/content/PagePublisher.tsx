51144695eea1566c006deee70ff1174945ca78ea
Paul-Erik Törrönen <poltsi@poltsi.fi>
2024-06-07 23:21:06 +0300

    Show potential galleries in the page publishing view
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/content/PagePublisher.tsx
+++ src/content/PagePublisher.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { validateParamId } from "../tools";
-import { ActionResult, SubmitResult } from "../models";
+import { ActionResult, QueryDetailEnum, SubmitResult } from "../models";
 import { PageVO } from "../models/Responses";
 import { pageAPI } from "../services";
-import { Button, Space, Spin } from "antd";
+import { Button, Space, Spin, Table } from "antd";
 import { SubmitResultHandler } from "../main";
 import TextArea from "antd/es/input/TextArea";
+import { galleryAPI } from "../services/Files";
+import { GalleryVO } from "../models/Responses/Files";
 
 export function PagePublisher() {
     const {paramId} = useParams();
@@ -15,6 +17,22 @@ export function PagePublisher() {
     const [loadResults, setLoadResults] = useState<SubmitResult>({status: ActionResult.NO_CHANGE, message: ""});
     const [submitResults, setSubmitResults] = useState<SubmitResult>({status: ActionResult.NO_CHANGE, message: ""});
     const [page, setPage] = useState<PageVO | null>(null);
+    const [galleryList, setGalleryList] = useState<GalleryVO[]>([]);
+
+    const galleryColumns = [
+        {
+            title: "Galleries that will be published with the page",
+            dataIndex: "short_name",
+            key: "title"
+        },
+        {title: "Description", dataIndex: "description", key: "description"},
+        {
+            title: "Files",
+            dataIndex: "common_files",
+            key: "common_files",
+            render: (files: number[]) => files.length
+        }
+    ];
 
     useEffect(() => {
         let tmpPageId: number = validateParamId(paramId);
@@ -30,18 +48,21 @@ export function PagePublisher() {
         setFpageId(tmpPageId);
 
         setLoading(true);
-        pageAPI.findById(tmpPageId, null)
-                .then((response) => {
-                    setPage(response);
+        Promise.all([
+            pageAPI.findById(tmpPageId, null),
+            galleryAPI.findAllByPage({details: QueryDetailEnum.MINIMAL}, tmpPageId)
+        ])
+                .then((responses) => {
+                    setPage(responses[0]);
+                    setGalleryList(responses[1]);
                 })
                 .catch((error) => {
-                    console.error("Error fetching page details:", error);
+                    console.error("Error fetching page details or list of galleries:", error);
                     setLoadResults({status: ActionResult.FAIL, message: "Failed to fetch the page details, try again later"});
                 })
                 .finally(() => {
-                            setLoading(false);
-                        }
-                );
+                    setLoading(false);
+                });
     }, [paramId]);
 
     function publishPage() {
@@ -79,6 +100,10 @@ export function PagePublisher() {
                                       style={{width: 800, height: "100%"}}
                                       autoSize={true}
                             />
+                            {galleryList.length > 0 && <Table key={"galleryList"}
+                                                              columns={galleryColumns}
+                                                              dataSource={galleryList}
+                                                              pagination={false}/>}
                             <Button key={"publishButton"} type={"primary"} onClick={publishPage}>Publish page</Button>
                         </Space>
                     </div>}
