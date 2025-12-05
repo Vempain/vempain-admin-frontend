aa136ccf787d9a1431a7ed6a11f5da9d14a07fe0
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-12-05 19:58:15 +0200

    Refresh yarn.lock Remove obsolete @ant-design/v5-patch-for-react-19 package
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter} from "react-router-dom";
-import '@ant-design/v5-patch-for-react-19';
 import {SessionProvider} from "@vempain/vempain-auth-frontend";
 
 const root = ReactDOM.createRoot(
